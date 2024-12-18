1a251ac36da29e0542399943a8728020a85072e8
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-18 12:30:19 +0100

    Add project files.

---
+++ Fraga_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Fraga_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+                       
+            Console.ReadKey();
+        }
+    }
+}
